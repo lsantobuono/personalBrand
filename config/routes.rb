ccfa858cc2ee0fe29d5349b19977b93ae23c145b
lsantobuono <luciano.santobuono@gmail.com>
2018-04-09 17:59:04 -0300

    Freelance

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get  '/home', to: 'static_pages#home'
-  get  '/contact', to: 'static_pages#contact'
   get  '/freelance', to: 'static_pages#freelance'
 
 
+  get  '/contact', to: 'static_pages#contact'
   get  '/blog', to: 'static_pages#blog'
 
 	resources :messages, only: [:new, :create]
